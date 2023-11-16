8d49ae2dbae7e84d3d088f05e2ed0341cf979b93
mrohrweck <monika.rohrweck@htl-steyr.ac.at>
2023-11-16 13:51:23 +0100

    add JUnit Test

--- src/main/java/DeepThoughtTest.java
+++ src/main/java/DeepThoughtTest.java
@@ -5,7 +5,7 @@ class DeepThoughtTest {
     @org.junit.jupiter.api.Test
     void answer_to_the_ultimate_question_of_life_the_universe_and_everything() {
         DeepThought superComputer = new DeepThought();
-        assertEquals( 42 , superComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything());
+        assertEquals( 42, superComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything());
 
     }
 }
